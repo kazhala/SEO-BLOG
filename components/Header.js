be1f7a8664fc0d0a57f60857bc656131a2baeaf3
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-11-07 10:24:58 +1100

    added nprogress to display loading pages

--- components/Header.js
+++ components/Header.js
@@ -1,79 +1,84 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
-	Collapse,
-	Navbar,
-	NavbarToggler,
-	NavbarBrand,
-	Nav,
-	NavItem,
-	NavLink
-} from 'reactstrap';
-import { APP_NAME } from '../config';
-import Link from 'next/link';
-import { signout, isAuth } from '../actions/auth';
-import Router from 'next/router';
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+import { APP_NAME } from "../config";
+import Link from "next/link";
+import { signout, isAuth } from "../actions/auth";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.onRouteChangeStart = url => NProgress.start();
+Router.onRouteChangeComplete = url => NProgress.done();
+Router.onRouteChangeError = url => NProgress.done();
 
 const Header = props => {
-	const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-	const toggle = () => setIsOpen(!isOpen);
+  const toggle = () => setIsOpen(!isOpen);
 
-	return (
-		<div>
-			<Navbar color="light" light expand="md">
-				<Link href="/">
-					<NavLink className="font-weight-bold">{APP_NAME}</NavLink>
-				</Link>
-				<NavbarToggler onClick={toggle} />
-				<Collapse isOpen={isOpen} navbar>
-					<Nav className="ml-auto" navbar>
-						{!isAuth() && (
-							<React.Fragment>
-								<NavItem>
-									<Link href="/signin">
-										<NavLink>Sign In</NavLink>
-									</Link>
-								</NavItem>
-								<NavItem>
-									<Link href="/signup">
-										<NavLink>Sign Up</NavLink>
-									</Link>
-								</NavItem>
-							</React.Fragment>
-						)}
-						{isAuth() && isAuth().role === 1 && (
-							<NavItem>
-								<Link href="/admin">
-									<NavLink>{`${isAuth().name}'s DashBoard`}</NavLink>
-								</Link>
-							</NavItem>
-						)}
-						{isAuth() && isAuth().role === 0 && (
-							<NavItem>
-								<Link href="/user">
-									<NavLink>{`${isAuth().name}'s DashBoard`}</NavLink>
-								</Link>
-							</NavItem>
-						)}
-						{isAuth() && (
-							<NavItem>
-								<NavLink
-									style={{ cursor: 'pointer' }}
-									onClick={() =>
-										signout(() => {
-											Router.replace('/signin');
-										})
-									}
-								>
-									Sign Out
-								</NavLink>
-							</NavItem>
-						)}
-					</Nav>
-				</Collapse>
-			</Navbar>
-		</div>
-	);
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <Link href="/">
+          <NavLink className="font-weight-bold">{APP_NAME}</NavLink>
+        </Link>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            {!isAuth() && (
+              <React.Fragment>
+                <NavItem>
+                  <Link href="/signin">
+                    <NavLink>Sign In</NavLink>
+                  </Link>
+                </NavItem>
+                <NavItem>
+                  <Link href="/signup">
+                    <NavLink>Sign Up</NavLink>
+                  </Link>
+                </NavItem>
+              </React.Fragment>
+            )}
+            {isAuth() && isAuth().role === 1 && (
+              <NavItem>
+                <Link href="/admin">
+                  <NavLink>{`${isAuth().name}'s DashBoard`}</NavLink>
+                </Link>
+              </NavItem>
+            )}
+            {isAuth() && isAuth().role === 0 && (
+              <NavItem>
+                <Link href="/user">
+                  <NavLink>{`${isAuth().name}'s DashBoard`}</NavLink>
+                </Link>
+              </NavItem>
+            )}
+            {isAuth() && (
+              <NavItem>
+                <NavLink
+                  style={{ cursor: "pointer" }}
+                  onClick={() =>
+                    signout(() => {
+                      Router.replace("/signin");
+                    })
+                  }
+                >
+                  Sign Out
+                </NavLink>
+              </NavItem>
+            )}
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
