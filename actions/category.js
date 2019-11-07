9cf4fda7edabc230fa7d1cd969e4261d7decfc88
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-11-07 15:24:27 +1100

    fetching all categories and allow deletion of categories

--- actions/category.js
+++ actions/category.js
@@ -1,16 +1,62 @@
-import fetch from "isomorphic-fetch";
-import { API } from "../config";
+import fetch from 'isomorphic-fetch';
+import { API } from '../config';
 
 export const create = async (category, token) => {
   try {
     let res = await fetch(`${API}/category`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
       },
-      body: JSON.stringify(category)
+      body: JSON.stringify(category),
+    });
+    return res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getCategories = async () => {
+  try {
+    let res = await fetch(`${API}/categories`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+    return res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const singleCategory = async slug => {
+  try {
+    let res = await fetch(`${API}/category/${slug}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+    return res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const removeCategory = async (slug, token) => {
+  try {
+    let res = await fetch(`${API}/category/${slug}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
     });
     return res.json();
   } catch (err) {
--- components/crud/Category.js
+++ components/crud/Category.js
@@ -2,7 +2,7 @@ import { useReducer, useEffect } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 import { isAuth, getCookie } from '../../actions/auth';
-import { create } from '../../actions/category';
+import { create, getCategories, removeCategory } from '../../actions/category';
 
 const initalState = {
   name: '',
@@ -10,6 +10,7 @@ const initalState = {
   success: false,
   categories: [],
   removed: false,
+  reload: false,
 };
 
 const reducer = (state, action) => {
@@ -20,12 +21,37 @@ const reducer = (state, action) => {
         name: action.payload,
         error: false,
         success: false,
-        removed: '',
+        removed: false,
       };
     case 'error':
-      return { ...state, error: action.payload, success: false };
+      return {
+        ...state,
+        error: action.payload,
+        success: false,
+        removed: false,
+      };
     case 'success':
-      return { ...state, error: false, success: true, name: '' };
+      return {
+        ...state,
+        error: false,
+        success: true,
+        name: '',
+        removed: false,
+        reload: !state.reload,
+      };
+    case 'categories':
+      return { ...state, categories: action.payload };
+    case 'removed':
+      return {
+        ...state,
+        error: false,
+        success: false,
+        name: '',
+        removed: true,
+        reload: !state.reload,
+      };
+    case 'clear':
+      return { ...state, error: '', success: false, removed: false };
     default:
       return state;
   }
@@ -33,14 +59,31 @@ const reducer = (state, action) => {
 
 const Category = () => {
   const [categoryState, dispatch] = useReducer(reducer, initalState);
-  const { name, error, success, categories, removed } = categoryState;
+  const { name, error, success, categories, removed, reload } = categoryState;
   const token = getCookie('token');
+
+  useEffect(() => {
+    const loadCategories = () => {
+      getCategories().then(res => {
+        console.log(res);
+        if (res.error) {
+          console.log(res.error);
+          dispatch({ type: 'error', payload: res.error });
+        } else {
+          dispatch({ type: 'categories', payload: res });
+        }
+      });
+    };
+
+    // console.log('re0render');
+    loadCategories();
+  }, [reload]);
 
   const handleSubmit = e => {
     e.preventDefault();
     // console.log("create category", name);
     create({ name }, token).then(res => {
-      console.log(res);
+      // console.log(res);
       if (res.error) {
         dispatch({ type: 'error', payload: res.error });
       } else {
@@ -51,6 +94,41 @@ const Category = () => {
 
   const handleChange = e => {
     dispatch({ type: 'name', payload: e.target.value });
+  };
+
+  const deleteCategory = slug => {
+    // console.log('delete', slug);
+    removeCategory(slug, token).then(res => {
+      if (res.error) {
+        dispatch({ type: 'error', payload: res.error });
+      } else {
+        dispatch({ type: 'removed' });
+      }
+    });
+  };
+
+  const deleteConfirm = slug => {
+    let answer = window.confirm(
+      'Are you sure you want to delete this category?'
+    );
+    if (answer) {
+      deleteCategory(slug);
+    }
+  };
+
+  const showCategories = () => {
+    return categories.map((cat, index) => {
+      return (
+        <button
+          onDoubleClick={() => deleteConfirm(cat.slug)}
+          title="Double click to delete"
+          key={index}
+          className="btn btn-outline-primary mr-1 ml-1 mt-3"
+        >
+          {cat.name}
+        </button>
+      );
+    });
   };
 
   const newCategoryForm = () => (
@@ -71,7 +149,40 @@ const Category = () => {
     </form>
   );
 
-  return <React.Fragment>{newCategoryForm()}</React.Fragment>;
+  const showSuccess = () => {
+    if (success) {
+      return <p className="text-success">Category is created</p>;
+    }
+  };
+
+  const showError = () => {
+    if (error) {
+      return <p className="text-danger">Category already exists</p>;
+    }
+  };
+
+  const showRemoved = () => {
+    if (removed) {
+      return <p className="text-danger">Category is removed</p>;
+    }
+  };
+
+  const mouseMoveHandler = e => {
+    dispatch({ type: 'clear' });
+  };
+
+  return (
+    <React.Fragment>
+      {showSuccess()}
+      {showError()}
+      {showRemoved()}
+
+      <div onMouseMove={mouseMoveHandler}>
+        {newCategoryForm()}
+        {showCategories()}
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Category;
