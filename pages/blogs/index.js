be8a3bcdcfb0a814f08b31878fd2298a2d483534
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-11-11 15:21:12 +1100

    added meta header for blog page (seo)

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,15 +1,48 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { withRouter } from 'next/router';
 import Layout from '../../components/Layout';
 import { useReducer } from 'react';
 import { listBlogsWithCategoriesAndTags } from '../../actions/blog';
-import { API } from '../../config';
 import moment from 'moment';
 import renderHTML from 'react-render-html';
 import Card from '../../components/blog/Card';
+import { API, DOMAIN, APP_NAME, FB_APP_ID } from '../../config';
 
 const Blogs = props => {
-  const { blogs, categories, tags, size } = props;
+  const { blogs, categories, tags, size, router } = props;
+
+  const head = () => (
+    <Head>
+      <title>Programming blogs | {APP_NAME}</title>
+      <meta
+        name="description"
+        content="Programming blogs and tutorials on react node next vue php laravel and web developemnt"
+      />
+      <link rel="canonical" href={`${DOMAIN}${router.pathname}`} />
+      <meta
+        property="og:title"
+        content={`Latest web development tutorials | ${APP_NAME}`}
+      />
+      <meta
+        property="og:description"
+        content="Programming blogs and tutorials on react node next vue php laravel and web developemnt"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={`${DOMAIN}${router.pathname}`} />
+      <meta property="og:site_name" content={`${APP_NAME}`} />
+      <meta
+        property="og:image"
+        content={`${DOMAIN}/static/images/seo-blog.png`}
+      />
+      <meta
+        property="og:image:secure_url"
+        content={`${DOMAIN}/static/images/seo-blog.png`}
+      />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="fb:app_id" content={`${FB_APP_ID}`} />
+    </Head>
+  );
 
   const showAllBlogs = () => {
     return blogs.map((blog, index) => (
@@ -37,31 +70,34 @@ const Blogs = props => {
   };
 
   return (
-    <Layout>
-      <main>
-        <div className="container-fluid">
-          <header>
-            <div className="col-md-12 pt-3">
-              <h1 className="display-4 font-weight-bold text-center">
-                Programming blogs and tutorials
-              </h1>
-            </div>
-            <section>
-              <div className="pb-5 text-center">
-                {showAllCateogires()}
-                <br />
-                {showAllTags()}
+    <React.Fragment>
+      {head()}
+      <Layout>
+        <main>
+          <div className="container-fluid">
+            <header>
+              <div className="col-md-12 pt-3">
+                <h1 className="display-4 font-weight-bold text-center">
+                  Programming blogs and tutorials
+                </h1>
               </div>
-            </section>
-          </header>
-        </div>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-12">{showAllBlogs()}</div>
+              <section>
+                <div className="pb-5 text-center">
+                  {showAllCateogires()}
+                  <br />
+                  {showAllTags()}
+                </div>
+              </section>
+            </header>
           </div>
-        </div>
-      </main>
-    </Layout>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-12">{showAllBlogs()}</div>
+            </div>
+          </div>
+        </main>
+      </Layout>
+    </React.Fragment>
   );
 };
 
@@ -81,4 +117,4 @@ Blogs.getInitialProps = () => {
   });
 };
 
-export default Blogs; //getInitialProps
+export default withRouter(Blogs); //getInitialProps
