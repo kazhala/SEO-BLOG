55878444358e447b7a3f754ac12cd07fa9bf8bb7
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-11-14 08:32:37 +1100

    fetch single blog in update blog page

--- components/crud/BlogUpdate.js
+++ components/crud/BlogUpdate.js
@@ -6,17 +6,50 @@ import { withRouter } from 'next/router';
 import { getCookie, isAuth } from '../../actions/auth';
 import { getCategories } from '../../actions/category';
 import { getTags } from '../../actions/tag';
-import { createBlog } from '../../actions/blog';
+import { singleBlog, updateBlog } from '../../actions/blog';
 const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 import '../../node_modules/react-quill/dist/quill.snow.css';
 import { Quillformats, Quillmodules } from '../../helpers/quill';
 
-const BlogUpdate = () => {
+const initialState = {
+  blog: {},
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'blog':
+      return { ...state, blog: action.payload };
+    default:
+      return state;
+  }
+};
+
+const BlogUpdate = props => {
+  const { router } = props;
+  const [blogState, dispatch] = useReducer(reducer, initialState);
+  const { blog } = blogState;
+
+  useEffect(() => {
+    const initBlog = () => {
+      if (router.query.slug) {
+        singleBlog(router.query.slug).then(data => {
+          if (data.error) {
+            console.log(data.error);
+          } else {
+            dispatch({ type: 'blog', payload: data });
+          }
+        });
+      }
+    };
+
+    initBlog();
+  }, [router]);
+
   return (
     <div className="container-fluid pb-5">
       <div className="row">
         <div className="col-md-8">
-          <p>create blog form</p>
+          {JSON.stringify(blog)}
           <div className="pt-3">show success and error msg</div>
         </div>
         <div className="col-md-4">
@@ -31,4 +64,4 @@ const BlogUpdate = () => {
   );
 };
 
-export default BlogUpdate;
+export default withRouter(BlogUpdate);
