cdd99f3a0c86e1c0ef5464e9789c19c25ff35c27
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-11-14 09:31:02 +1100

    display all categories and tags in updateblog

--- components/crud/BlogUpdate.js
+++ components/crud/BlogUpdate.js
@@ -213,16 +213,39 @@ const BlogUpdate = props => {
         <div className="col-md-8">
           {updateBlogForm()}
           <div className="pt-3">show success and error msg</div>
-          <hr />
-          {JSON.stringify(categories)}
-          <hr />
-          {JSON.stringify(tags)}
         </div>
         <div className="col-md-4">
           <div>
             <div className="form-group pb-2">
               <h5>Featured image</h5>
+              <hr />
+
+              <small className="text-muted">Max size: 1mb</small>
+              <label className="btn btn-outline-info">
+                Upload featured image
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={e => handleChange(e, 'photo')}
+                  hidden
+                />
+              </label>
             </div>
+          </div>
+
+          <div>
+            <h5>Categories</h5>
+            <hr />
+            <ul style={{ maxHeight: '200px', overflowY: 'scroll' }}>
+              {showCategories()}
+            </ul>
+          </div>
+          <div>
+            <h5>Tags</h5>
+            <hr />
+            <ul style={{ maxHeight: '200px', overflowY: 'scroll' }}>
+              {showTags()}
+            </ul>
           </div>
         </div>
       </div>
