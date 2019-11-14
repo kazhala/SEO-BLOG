d08415159642a32212f5fcc64366f68c1b6b1ee3
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-11-14 14:52:01 +1100

    added head section for tag page

--- pages/tags/[slug].js
+++ pages/tags/[slug].js
@@ -6,11 +6,48 @@ import { API, DOMAIN, APP_NAME, FB_APP_ID } from '../../config';
 import moment from 'moment';
 import renderHTML from 'react-render-html';
 import Card from '../../components/blog/Card';
+import { withRouter } from 'next/router';
 
 const Tag = props => {
-  const { tag, blogs } = props;
+  const { query, tag, blogs, router } = props;
+
+  const head = () => {
+    return (
+      <Head>
+        <title>
+          {tag.name} | {APP_NAME}
+        </title>
+        <meta
+          name="description"
+          content={`Best programming tutorials on ${tag.name}`}
+        />
+        <link rel="canonical" href={`${DOMAIN}/tags/${query.slug}`} />
+        {/* below is for facebook link share data show */}
+        <meta property="og:title" content={`${tag.name} | ${APP_NAME}`} />
+        <meta
+          property="og:description"
+          content={`Best programming tutorials on ${tag.name}`}
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={`${DOMAIN}/tags/${query.slug}`} />
+        <meta property="og:site_name" content={`${APP_NAME}`} />
+        <meta
+          property="og:image"
+          content={`${DOMAIN}/static/images/seo-blog.png`}
+        />
+        <meta
+          property="og:image:secure_url"
+          content={`${DOMAIN}/static/images/seo-blog.png`}
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="fb:app_id" content={`${FB_APP_ID}`} />
+      </Head>
+    );
+  };
+
   return (
     <React.Fragment>
+      {head()}
       <Layout>
         <main>
           <div className="container-fluid text-center">
@@ -37,9 +74,9 @@ Tag.getInitialProps = ({ query }) => {
     if (data.error) {
       console.log(data.error);
     } else {
-      return { tag: data.tag, blogs: data.blogs };
+      return { query, tag: data.tag, blogs: data.blogs };
     }
   });
 };
 
-export default Tag;
+export default withRouter(Tag);
