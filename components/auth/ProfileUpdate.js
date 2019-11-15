0388b363e8ec0c109e2a3138b56f8fb10847bff6
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-11-15 17:25:51 +1100

    added form for update profile page

--- components/auth/ProfileUpdate.js
+++ components/auth/ProfileUpdate.js
@@ -46,8 +46,9 @@ const ProfileUpdate = () => {
     error,
     success,
     loading,
-    photot,
+    photo,
     userData,
+    about,
   } = updateState;
 
   useEffect(() => {
@@ -60,15 +61,86 @@ const ProfileUpdate = () => {
     });
   }, [token]);
 
+  const handleChange = (e, name) => {};
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  const profileUpdateForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="btn btn-outline-info">
+          Profile photo
+          <input
+            type="file"
+            accept="image/*"
+            onChange={e => handleChange(e, 'photo')}
+            hidden
+          />
+        </label>
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Username</label>
+        <input
+          type="text"
+          className="form-control"
+          value={username}
+          onChange={e => handleChange(e, 'username')}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">name</label>
+        <input
+          type="text"
+          className="form-control"
+          value={name}
+          onChange={e => handleChange(e, 'name')}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">email</label>
+        <input
+          type="text"
+          className="form-control"
+          value={email}
+          onChange={e => handleChange(e, 'email')}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">About</label>
+        <textarea
+          type="text"
+          className="form-control"
+          value={about}
+          onChange={e => handleChange(e, 'about')}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">password</label>
+        <input
+          type="text"
+          className="form-control"
+          value={password}
+          onChange={e => handleChange(e, 'password')}
+        />
+      </div>
+      <div>
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+
   return (
     <React.Fragment>
       <div className="container">
         <div className="row">
           <div className="col-md-4">image</div>
-          <div className="col-md-8">
-            update form
-            {JSON.stringify({ username, email, name })}
-          </div>
+          <div className="col-md-8 mb-5">{profileUpdateForm()}</div>
         </div>
       </div>
     </React.Fragment>
