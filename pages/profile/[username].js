fe3f027ba78497abc894960080c31885f8d1b3c6
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-11-15 14:55:32 +1100

    added components for profile page

--- pages/profile/[username].js
+++ pages/profile/[username].js
@@ -5,7 +5,10 @@ import Layout from '../../components/Layout';
 import { DOMAIN, APP_NAME, FB_APP_ID } from '../../config';
 import moment from 'moment';
 
-const UserProfile = () => {
+const UserProfile = props => {
+  const { user, blogs } = props;
+  console.log(user);
+
   return (
     <React.Fragment>
       <Layout>
@@ -14,8 +17,41 @@ const UserProfile = () => {
             <div className="col-md-12">
               <div className="card">
                 <div className="card-body">
-                  <h5>username</h5>
-                  <p>...</p>
+                  <h5>{user.name}</h5>
+                  <Link href={`${user.profile}`}>
+                    <a>View Profile</a>
+                  </Link>
+                  <p className="text-muted">
+                    Joined {moment(user.createdAt).fromNow()}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <br />
+        <div className="container pd-5">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title bg-primary pt-4 pb-4 pl-4 pr-4">
+                    Recent blogs by {user.name}
+                  </h5>
+                  <br />
+                  <p>show user blogs</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title bg-primary pt-4 pb-4 pl-4 pr-4">
+                    Message {user.name}
+                  </h5>
+                  <br />
+                  <p>contact form</p>
                 </div>
               </div>
             </div>
@@ -24,6 +60,16 @@ const UserProfile = () => {
       </Layout>
     </React.Fragment>
   );
+};
+
+UserProfile.getInitialProps = ({ query }) => {
+  return userPublicProfile(query.username).then(data => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      return { user: data.user, blogs: data.blogs };
+    }
+  });
 };
 
 export default UserProfile;
