7486a85d600f3d6616d7d95b2118edae57df3120
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-11-15 13:38:20 +1100

    fetch search data

--- components/blog/Search.js
+++ components/blog/Search.js
@@ -4,14 +4,24 @@ import { useReducer, useEffect } from 'react';
 import { listSearch } from '../../actions/blog';
 
 const initialState = {
-  search: undefined,
-  resultes: [],
+  search: '',
+  results: [],
   searched: false,
   message: '',
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
+    case 'inputSearch':
+      return { ...state, search: action.payload, searched: false, results: [] };
+    case 'results':
+      return {
+        ...state,
+        search: '',
+        results: action.payload,
+        searched: true,
+        message: `${action.payload.length} blogs are found`,
+      };
     default:
       return state;
   }
@@ -19,13 +29,26 @@ const reducer = (state, action) => {
 
 const Search = () => {
   const [searchState, dispatch] = useReducer(reducer, initialState);
-  const { search, resultes, searched, message } = searchState;
+  const { search, results, searched, message } = searchState;
 
   const searchSubmit = e => {
     e.preventDefault();
+    listSearch({ search }).then(data => {
+      dispatch({ type: 'results', payload: data });
+    });
   };
 
-  const handleChange = e => {};
+  const handleChange = e => {
+    dispatch({ type: 'inputSearch', payload: e.target.value });
+  };
+
+  const searchedBlogs = results => {
+    return (
+      <div className="jumbotron bg-white">
+        {message && <p className="pt-4 text-muted font-italic">{message}</p>}
+      </div>
+    );
+  };
 
   const searchForm = () => (
     <form onSubmit={searchSubmit}>
@@ -36,6 +59,7 @@ const Search = () => {
             className="form-control"
             placeholder="Search blogs"
             onChange={handleChange}
+            value={search}
           />
         </div>
         <div className="col-md-4">
