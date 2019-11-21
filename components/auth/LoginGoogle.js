56972a802da5d5870fa3e13c9cf768e95fc4ce1b
kevin zhuang <kevin7441@gmail.com>
2019-11-21 15:18:27 +1100

    success login with google

--- components/auth/LoginGoogle.js
+++ components/auth/LoginGoogle.js
@@ -1,13 +1,27 @@
-import Link from 'next/link';
 import { useReducer, useEffect } from 'react';
 import Router from 'next/router';
-import { loginWithGoogle } from '../../actions/auth';
+import { loginWithGoogle, authenticate, isAuth } from '../../actions/auth';
 import { GOOGLE_CLIENT_ID } from '../../config';
 import GoogleLogin from 'react-google-login';
 
 const LoginGoogle = () => {
-  const responseGoogle = res => {
-    console.log(res);
+  const responseGoogle = response => {
+    const tokenId = response.tokenId;
+    const user = { tokenId };
+
+    loginWithGoogle(user).then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        authenticate(data, () => {
+          if (isAuth() && isAuth().role === 1) {
+            Router.push('/admin');
+          } else {
+            Router.push('/user');
+          }
+        });
+      }
+    });
   };
 
   return (
