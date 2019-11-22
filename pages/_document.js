a3e389f6c1c10c75ad4680ca3c7d0bebe0a8a40d
kevin zhuang <kevin7441@gmail.com>
2019-11-22 16:49:09 +1100

    added google analytics script

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,26 @@
 // Event handlers like onClick can't be added to this file
 
 // ./pages/_document.js
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
 
 class MyDocument extends Document {
+  setGoogleTags() {
+    if (publicRuntimeConfig.PRODUCTION) {
+      return {
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-153077299-1');
+
+        `,
+      };
+    }
+  }
+
   render() {
     return (
       <Html lang="en">
@@ -19,6 +36,11 @@ class MyDocument extends Document {
             href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css"
           />
           <link rel="stylesheet" href="/static/css/styles.css" />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-153077299-1"
+          ></script>
+          <script dangerouslySetInnerHTML={this.setGoogleTags()}></script>
         </Head>
         <body>
           <Main />
